78be201ce1035f0191800d58a69325b67653fdb0
Taine Jarvis <tainejarvis.dev@gmail.com>
2024-08-09 16:46:27 +0100

    Added fetching JSON from server logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import MainLayout from './layouts/MainLayout'
+import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const App = () => {
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+
+  <Route path = '/' element = {<MainLayout />}> 
+    <Route index element = {<HomePage />} /> 
+    <Route path = '/jobs' element = {<JobsPage />} />
+    <Route path = '*' element = {<NotFoundPage />} />
+  </Route>
+  
+  )
+)
+
   return (
-    <>
-    <Navbar/>
-
-    <Hero/>
-
-    <HomeCards />
-
-    <JobListings/>
-
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
-    </>
+    <RouterProvider router = {router} />
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import logo from '../assets/images/logo.png'
+import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
+
+  const linkClass = ({isActive}) => 
+  isActive ? "text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2" : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -10,7 +15,7 @@ const Navbar = () => {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
             {/* <!-- Logo --> */}
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
               <img
                 className="h-10 w-auto"
                 src={logo}
@@ -19,23 +24,23 @@ const Navbar = () => {
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >React Jobs</span
               >
-            </a>
+            </NavLink>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <a
-                  href="/index.html"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
+                <NavLink
+                  to="/"
+                  className= {linkClass}
+                  >Home </NavLink
                 >
-                <a
-                  href="/jobs.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
+                <NavLink
+                  to="/jobs"
+                  className= {linkClass}
+                  >Jobs</NavLink
                 >
-                <a
-                  href="/add-job.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
+                <NavLink
+                  to="/add-job"
+                  className= {linkClass}
+                  >Add Job</NavLink
                 >
               </div>
             </div>
